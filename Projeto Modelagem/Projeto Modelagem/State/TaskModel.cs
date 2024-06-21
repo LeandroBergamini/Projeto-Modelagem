9b5b26bd4650d3d1094e2953a3483e328c1232c0
leandro.bergamini <leandro.ber2004@gmail.com>
2024-06-21 14:58:59 -0300

    Projeto Modelagem State API

---
+++ Projeto Modelagem/Projeto Modelagem/State/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿
+    using global::StateAPI.State.Enum;
+    using StateAPI.State.Enum;
+
+namespace StateAPI.State
+{
+    public class TasksModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public StateTask State { get; set; }
+    }
+}
+    
